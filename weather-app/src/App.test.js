71a9545ad9f1a04e9722fda4f92d198f27dea4b6
Max Staite <max.staite2004@gmail.com>
2023-07-31 22:33:28 +0100

    p

--- weather-app/src/App.test.js
+++ weather-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Weather App H1 element', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const H1Element = screen.getByText(/Weather App/i);
+  expect(H1Element).toBeInTheDocument();
 });
