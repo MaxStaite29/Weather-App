6ce0ac5411921caec94374d06ad0394adf608efd
Max Staite <129111863+MaxStaite29@users.noreply.github.com>
2023-07-31 22:36:19 +0100

    Delete music-library directory

--- music-library/migrations/02-create-album-table.sql
+++
@@ -1,7 +0,0 @@
-CREATE  TABLE Albums (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    year INT NOT NULL,
-    artistId INT,
-    CONSTRAINT fk_artistId FOREIGN KEY(artistId) REFERENCES Artists(id)
-);
--- music-library/tests/helper.js
+++
@@ -1,5 +0,0 @@
-const db = require('../src/db');
-
-afterEach(async () => {
-    await db.query('TRUNCATE Artists CASCADE')
-});
