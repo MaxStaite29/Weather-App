ff12ce0b3c15524cf03a27671bccac4425457956
Max Staite <max.staite2004@gmail.com>
2023-08-04 20:26:43 +0100

    updated .gitignore

--- weather-app/src/App.test.js
+++ weather-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Weather App H1 element', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const H1Element = screen.getByText(/Weather App/i);
-  expect(H1Element).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
