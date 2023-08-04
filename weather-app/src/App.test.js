18306970490037b76f7bd7d3abc288cb6b7a79fc
Max Staite <max.staite2004@gmail.com>
2023-08-04 23:28:21 +0100

    project setup

--- weather-app/src/App.test.js
+++ weather-app/src/App.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "./App";
 
-test('renders text Weather App', () => {
+test("renders text Weather App", () => {
   render(<App />);
   const linkElement = screen.getByText(/Weather App/i);
   expect(linkElement).toBeInTheDocument();
